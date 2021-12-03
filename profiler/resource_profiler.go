5e8b1aaeca792bd1972b6c7827449dbf7670ed33
Suyi32 <lisuyi_imagine@outlook.com>
2021-12-03 16:31:34 +0800

    float32 -> float64

--- profiler/resource_profiler.go
+++ profiler/resource_profiler.go
@@ -12,24 +12,24 @@ import (
 )
 
 type ResourceProfiler struct {
-    lastCPU float32
-    Memo float32
-	lastTimeStamp float32
+    lastCPU float64
+    Memo float64
+	lastTimeStamp float64
 }
 
-func (resourceProfiler *ResourceProfiler) ReadCPU() float32 {
+func (resourceProfiler *ResourceProfiler) ReadCPU() float64 {
 	data, err := ioutil.ReadFile("/sys/fs/cgroup/cpuacct/cpuacct.usage")
 	if err != nil{
 			log.Fatal(err)
 	}
 	cpu_string := strings.TrimSuffix(string(data), "\n")
-	cpu_usage, err := strconv.ParseFloat(cpu_string, 32)
+	cpu_usage, err := strconv.ParseFloat(cpu_string, 64)
 	//fmt.Println(cpu_usage, err, reflect.TypeOf(cpu_usage))
 	
-	currentCPU := float32(cpu_usage)
-	currentTimeStamp := float32(time.Now().UnixNano())
+	currentCPU := float64(cpu_usage)
+	currentTimeStamp := float64(time.Now().UnixNano())
 
-	var res float32 = (currentCPU - resourceProfiler.lastTimeStamp) / (currentTimeStamp - resourceProfiler.lastTimeStamp)
+	var res float64 = (currentCPU - resourceProfiler.lastCPU) / (currentTimeStamp - resourceProfiler.lastTimeStamp)
 
 	resourceProfiler.lastTimeStamp = currentTimeStamp
 	resourceProfiler.lastCPU = currentCPU
@@ -37,9 +37,9 @@ func (resourceProfiler *ResourceProfiler) ReadCPU() float32 {
 	return res
 }
 
-func (resourceProfiler *ResourceProfiler) ReadMemo() float32 {
-	var memo_usage_in_bytes float32
-	var cache float32
+func (resourceProfiler *ResourceProfiler) ReadMemo() float64 {
+	var memo_usage_in_bytes float64
+	var cache float64
 
 	file, err := os.Open("/sys/fs/cgroup/memory/memory.usage_in_bytes")
 	if err !=  nil{
@@ -49,7 +49,7 @@ func (resourceProfiler *ResourceProfiler) ReadMemo() float32 {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 			_, err := fmt.Sscan(scanner.Text(), &memo_usage_in_bytes)
-			//strconv.ParseFloat(f, 32)
+			//strconv.ParseFloat(f, 64)
 			if err != nil{
 					log.Fatal(err)
 			}
@@ -73,7 +73,7 @@ func (resourceProfiler *ResourceProfiler) ReadMemo() float32 {
 		}
 	}
 	// fmt.Printf("cache=%f, type: %T\n", cache, cache)
-	var curMemo float32 = (memo_usage_in_bytes - cache) / 1048576.0
+	var curMemo float64 = (memo_usage_in_bytes - cache) / 1048576.0
 	resourceProfiler.Memo = curMemo
 	return curMemo
 }
@@ -84,11 +84,11 @@ func NewResourceProfiler() *ResourceProfiler {
 			log.Fatal(err)
 	}
 	cpu_string := strings.TrimSuffix(string(data), "\n")
-	cpu_usage, err := strconv.ParseFloat(cpu_string, 32)
+	cpu_usage, err := strconv.ParseFloat(cpu_string, 64)
 	
 	return &ResourceProfiler{
-		lastCPU: float32(cpu_usage),
-		Memo: float32(0.0),
-		lastTimeStamp: float32(time.Now().UnixNano()),
+		lastCPU: float64(cpu_usage),
+		Memo: float64(0.0),
+		lastTimeStamp: float64(time.Now().UnixNano()),
 	}
 }
